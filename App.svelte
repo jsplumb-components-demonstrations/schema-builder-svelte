9d8443a93208acd03445f9193dc98bdce6adeeb2
Simon Porritt <simon@jsplumbtoolkit.com>
2023-02-06 22:43:10 +1100

    initial import

---
+++ App.svelte
@@ -0,0 +1,25 @@
+
+<script>
+    import {onMount} from "svelte"
+    import {SchemaComponent, PaletteComponent, InspectorComponent} from "@jsplumb-components/schema-svelte"
+
+    let schemaBuilder;
+
+    onMount(async () => {
+        schemaBuilder.loadUrl("./schema-1.json", () => {
+            schemaBuilder.zoomToFit()
+        })
+    })
+
+</script>
+
+
+<div id="canvas">
+    <SchemaComponent id="mySchema" bind:this={schemaBuilder}/>
+</div>
+
+<div id="rhs">
+    <PaletteComponent schemaId="mySchema"></PaletteComponent>
+    <InspectorComponent schemaId="mySchema"></InspectorComponent>
+</div>
+
---
+++ main.js
@@ -0,0 +1,12 @@
+import App from './App.svelte';
+
+
+
+const app = new App({
+    target: document.getElementById("jtk-demo-container"),
+    props: {
+        // data
+    }
+});
+
+export default app;
